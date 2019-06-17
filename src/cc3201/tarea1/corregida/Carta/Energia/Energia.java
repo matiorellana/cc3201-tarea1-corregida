9b71e87ec2b43a7daa89a296c7aa01198291af01
matiorellana <m.orellana.arce@gmail.com>
2019-06-17 16:58:40 -0400

    Modificado metodo jugar, para adaptarse al nuevo requisito

--- src/cc3201/tarea1/corregida/Carta/Energia/Energia.java
+++ src/cc3201/tarea1/corregida/Carta/Energia/Energia.java
@@ -3,19 +3,25 @@ package cc3201.tarea1.corregida.Carta.Energia;
 import cc3201.tarea1.corregida.Entrenador;
 import cc3201.tarea1.corregida.Carta.Pokemon.Pokemon;
 
+import java.util.Scanner;
+
 public abstract class Energia implements IEnergia {
 
     protected String nombre;
+    protected Scanner scanner;
 
     protected Energia(String nombre){
 
         this.nombre = nombre;
+        scanner = new Scanner(System.in);
     }
 
     @Override
     public void jugar(Entrenador entrenador) {
 
-        entrenador.getPokemonActivo().addEnergia(this);
+        System.out.println("Ingresa numero para elegir pokemon de la banca:");
+        int index = scanner.nextInt();
+        entrenador.getBanca().get(index).addEnergia(this);
     }
 
     @Override
