9c5f95e37af0ccb092e5f824c3d79298d52f6618
matiorellana <m.orellana.arce@gmail.com>
2019-06-17 08:35:09 -0400

    Creados clases para pokemon fase 1, movidos a package Fase1

---
+++ src/cc3201/tarea1/corregida/Carta/Pokemon/Fase1/PokemonFase1Lucha.java
@@ -0,0 +1,28 @@
+package cc3201.tarea1.corregida.Carta.Pokemon.Fase1;
+
+import cc3201.tarea1.corregida.Ataque.Ataque;
+import cc3201.tarea1.corregida.Carta.Pokemon.IPokemon;
+import cc3201.tarea1.corregida.Carta.Pokemon.PokemonLucha;
+import cc3201.tarea1.corregida.Carta.Pokemon.VisitorPokemon;
+import cc3201.tarea1.corregida.Entrenador;
+
+import java.util.ArrayList;
+
+public class PokemonFase1Lucha extends PokemonLucha implements IPokemon, PokemonFase1 {
+
+    public PokemonFase1Lucha(String nombre, int id, int salud, ArrayList<Ataque> ataques){
+
+        super(nombre, id, salud, ataques);
+    }
+
+    @Override
+    public void play(Entrenador entrenador) {
+
+        this.accept(this.visitor, entrenador);
+    }
+
+    @Override
+    public void accept(VisitorPokemon visitor, Entrenador entrenador) {
+        visitor.visitorFase(this, entrenador);
+    }
+}
